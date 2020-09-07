3502fda8f387e0bc5766f592e4a97a8a0b2560e0
Matheus Wilhelms de Souza <ma_theus_ws@hotmail.com>
2020-09-07 17:31:40 -0300

    Reestruturação das pastas e Exemplo.sln

---
+++ Tarefa02/Exemplo/Motosserra.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "ArmaBase.h"
+class Motosserra : public ArmaBase
+{
+public:
+	Motosserra();
+	~Motosserra() {};
+};
---
+++ Tarefa02/Exemplo/Pistola.cpp
@@ -0,0 +1,7 @@
+#include "Pistola.h"
+
+Pistola::Pistola()
+{
+	id = "PISTOLA";
+	maxAmmo = ammo = 12;
+}
