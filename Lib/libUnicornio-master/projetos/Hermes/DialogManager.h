e658b71c3c3d1aaa474951568d31d3dd9c2c3455
Matheus Wilhelms de Souza <ma_theus_ws@hotmail.com>
2020-10-14 13:29:38 -0300

    att

---
+++ Lib/libUnicornio-master/projetos/Hermes/DialogManager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Lista.h"
+class DialogManager
+{
+public:
+
+private:
+	Lista<Dialog*> dialogs;
+
+};
+
--- Lib/libUnicornio-master/projetos/Hermes/No.h
+++ Lib/libUnicornio-master/projetos/Hermes/No.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Dialog.h"
+
 template <class T>
 class No
 {
